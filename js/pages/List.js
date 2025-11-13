349512aabe30212c3185b67055969a80f686cb94
Lucidium <118640164+iiLogan@users.noreply.github.com>
2025-11-13 14:15:49 -0800

    dsadas

--- js/pages/List.js
+++ js/pages/List.js
@@ -74,8 +74,8 @@ export default {
                     </ul>
                     <h2>Records</h2>
                     <p v-if="level.rank === null">This level does not accept records.</p>
-                    <p v-else-if="level.rank <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
-                    <p v-else-if="level.rank <= 150"><strong>100%</strong> or better to qualify</p>
+                    <p v-else-if="level.rank <= 100"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-else-if="level.rank <= 200"><strong>100%</strong> or better to qualify</p>
                     <p v-else>This level has fallen to legacy, but still accepts completion records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
